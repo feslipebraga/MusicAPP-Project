c11af70df7b4a81a860161d5534433eb73e4687a
feslipebraga <feslipebraga@gmail.com>
2023-10-13 16:58:38 -0300

    first commit

---
+++ DDL.sql
@@ -0,0 +1,23 @@
+CREATE DOMAIN dm_desc50 varchar(50);
+
+CREATE TABLE musicas(
+    id serial primary key,
+    titulo dm_titulo,
+    duracao 
+
+)
+
+CREATE TABLE artistas(
+    id serial primary key,
+    nome dm_titulo,
+)
+
+CREATE TABLE albuns(
+    id serial primary key,
+    
+)
+
+CREATE TABLE musicas(
+    id serial primary key,
+    
+)
