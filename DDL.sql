8cbe00c603823263048e8996c840d5197ab7eb5f
feslipebraga <feslipebraga@gmail.com>
2023-10-13 20:18:38 -0300

    atualizacoes

--- DDL.sql
+++ DDL.sql
@@ -1,23 +1,32 @@
-CREATE DOMAIN dm_desc50 varchar(50);
+CREATE DOMAIN dm_vc20 AS VARCHAR(20);
+CREATE DOMAIN dm_vc50 AS VARCHAR(50);
+CREATE DOMAIN dm_vc255 AS VARCHAR(255);
 
-CREATE TABLE musicas(
-    id serial primary key,
-    titulo dm_titulo,
-    duracao 
-
-)
-
+-- 	Nome, view: ouvintes mensais, view: músicas populares, view: discografia, view: playlists incluídas, view: albuns incluídos e biografia.
 CREATE TABLE artistas(
-    id serial primary key,
-    nome dm_titulo,
-)
+    id SERIAL PRIMARY KEY,
+    nome dm_vc50,
+    biografia dm_vc255
+);
 
+-- 	Título, view: lista de faixas, fk: artista, ano de lançamento, gênero e view: duração.
 CREATE TABLE albuns(
-    id serial primary key,
-    
-)
+    id SERIAL PRIMARY KEY,
+    titulo dm_vc50,
+    artistaID INTEGER,
+    genero dm_vc20,
+    anoLancamento YEAR
+    CHECK (anoLancamento <= EXTRACT(YEAR FROM CURRENT_DATE)),
+    FOREIGN KEY artistaID REFERENCES artistas(id) ON DELETE CASCADE
+);
 
+-- 	Título, duração, fk: artista, fk: álbum, join: gênero e join: ano de lançamento.
 CREATE TABLE musicas(
-    id serial primary key,
-    
-)
+    id SERIAL PRIMARY KEY,
+    titulo dm_vc50,
+    duracao interval,
+    artistaID INTEGER,
+    albumID INTEGER,
+    FOREIGN KEY (artistaID) references artistas(id) ON DELETE CASCADE,
+    FOREIGN KEY (albumID) references albuns(id) ON DELETE CASCADE
+);
