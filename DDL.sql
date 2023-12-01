0b6fafa5ec3b886836db58eee55fef3718e1c063
Felipe Braga <103913972+feslipebraga@users.noreply.github.com>
2023-12-01 16:28:40 -0300

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,22 +1,33 @@
-CREATE DOMAIN dm_vc20 AS VARCHAR(20);
-CREATE DOMAIN dm_vc50 AS VARCHAR(50);
-CREATE DOMAIN dm_vc255 AS VARCHAR(255);
+DROP TABLE IF EXISTS artistas;
+DROP TABLE IF EXISTS generos;
+DROP TABLE IF EXISTS albuns;
+DROP TABLE IF EXISTS musicas;
+DROP TABLE IF EXISTS playlists;
+DROP TABLE IF EXISTS usuarios;
+DROP TABLE IF EXISTS musicas_playlists;
+DROP TABLE IF EXISTS artistas_favoritados;
+DROP TABLE IF EXISTS follows;
+DROP TABLE IF EXISTS musicasEscutadas;
 
--- 	Nome, view: discografia, view: playlists incluídas, view: albuns incluídos e biografia.
+-- Criando os domínios
+CREATE DOMAIN dm_pequeno AS VARCHAR(20) NOT NULL;
+CREATE DOMAIN dm_medio AS VARCHAR(50) NOT NULL;
+CREATE DOMAIN dm_grande AS VARCHAR(100) NOT NULL;
+
+-- 	Criando as tabelas
 CREATE TABLE artistas(
     id SERIAL PRIMARY KEY,
-    nome dm_vc50
+    nome dm_medio UNIQUE
 );
 
 CREATE TABLE generos (
     id SERIAL PRIMARY KEY,
-    nome dm_vc20 UNIQUE
+    nome dm_pequeno UNIQUE
 );
 
--- 	Título, view: lista de faixas, fk: artista, ano de lançamento, gênero e view: duração.
 CREATE TABLE albuns(
     id SERIAL PRIMARY KEY,
-    titulo dm_vc50,
+    titulo dm_grande,
     artistaID INTEGER,
     generoID INTEGER,
     anoLancamento INTEGER
@@ -25,10 +36,9 @@ CREATE TABLE albuns(
     FOREIGN KEY (generoID) REFERENCES generos(id) ON DELETE CASCADE
 );
 
--- 	Título, duração, fk: artista, fk: álbum, join: gênero e join: ano de lançamento.
 CREATE TABLE musicas(
     id SERIAL PRIMARY KEY,
-    titulo dm_vc255,
+    titulo dm_grande,
     duracao interval,
     artistaID INTEGER,
     albumID INTEGER,
@@ -36,23 +46,50 @@ CREATE TABLE musicas(
     FOREIGN KEY (albumID) references albuns(id) ON DELETE CASCADE
 );
 
-CREATE TABLE playlists(
-    id SERIAL PRIMARY KEY,
-    nome dm_vc50
-);
-
 CREATE TABLE usuarios (
     id SERIAL PRIMARY KEY,
-    username dm_vc50,
-    email dm_vc255 UNIQUE,
-    senha dm_vc255,
+    username dm_medio UNIQUE,
+    email dm_grande UNIQUE,
+    senha dm_grande,
     dataRegistro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE musicas_playlists (
+CREATE TABLE playlists(
     id SERIAL PRIMARY KEY,
-    musicaID INTEGER,
-    playlistID INTEGER,
-    FOREIGN KEY (musicaID) REFERENCES musicas(id),
-    FOREIGN KEY (playlistID) REFERENCES playlists(id)
+    nome dm_grande,
+    criadorID INTEGER NOT NULL,
+    dataCriacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    dataModificacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (criadorID) references usuarios(id) ON DELETE CASCADE
+);
+
+CREATE TABLE musicas_playlists (
+    musicaID INTEGER NOT NULL,
+    playlistID INTEGER NOT NULL,
+    FOREIGN KEY (musicaID) REFERENCES musicas(id) ON DELETE CASCADE,
+    FOREIGN KEY (playlistID) REFERENCES playlists(id) ON DELETE CASCADE,
+    PRIMARY KEY (musicaID, playlistID)
+);
+
+CREATE TABLE artistas_favoritados (
+    usuarioID INTEGER NOT NULL,
+    artistaID INTEGER NOT NULL,
+    FOREIGN KEY (usuarioID) REFERENCES usuarios(id) ON DELETE CASCADE,
+    FOREIGN KEY (artistaID) REFERENCES artistas(id) ON DELETE CASCADE
+);
+
+CREATE TABLE follows (
+    seguidorID INTEGER NOT NULL,
+    seguidoID INTEGER NOT NULL,
+    dataFollow TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (seguidorID) REFERENCES usuarios(id) ON DELETE CASCADE,
+    FOREIGN KEY (seguidoID) REFERENCES usuarios(id) ON DELETE CASCADE
+);
+
+CREATE TABLE musicasEscutadas (
+    usuarioID INTEGER NOT NULL,
+    musicaID INTEGER NOT NULL,
+    dataReproducao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (usuarioID) REFERENCES usuarios(id) ON DELETE CASCADE,
+    FOREIGN KEY (musicaID) REFERENCES musicas(id) ON DELETE CASCADE
 );
